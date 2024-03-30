1ff119ac9af399cbde387dfaf42bd1439f3cf730
castrosgabriel <castro.sgabriel@gmail.com>
2024-03-30 00:58:37 -0300

    bug fix SliderPalestra primeiro slide bug

--- src/components/palestras/SliderPalestra.tsx
+++ src/components/palestras/SliderPalestra.tsx
@@ -1,7 +1,7 @@
 import { useGSAP } from '@gsap/react';
 import './Palestra.css';
 import gsap from 'gsap';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { ArrowGalery } from '../galery/Galery';
 import { SvgBlob, SvgPointer } from './SvgPointer';
 
@@ -19,8 +19,10 @@ const SliderPalestra = ({ itemsArray }: SliderPalestraProps) => {
     const imgRefs = useRef<Array<HTMLDivElement | null>>([]);
     const txtRefs = useRef<Array<HTMLDivElement | null>>([]);
     const [currentSlide, setCurrentSlide] = useState(0)
+    const [animating, setAnimating] = useState(false)
     const current = itemsArray[currentSlide];
     const currentColor = current?.color;
+    const contentRef = useRef<HTMLDivElement | null>(null)
 
     const animateTriangle = (reverse?: boolean) => {
         const triangleFirst = '#pointer-first';
@@ -61,132 +63,138 @@ const SliderPalestra = ({ itemsArray }: SliderPalestraProps) => {
 
     const animateSlide = (reverse?: boolean) => {
         const reverseValue = reverse ? -1 : 1;
-        gsap.to('.slider-content', {
+        gsap.to(contentRef.current, {
             x: `-=${(100 / itemsArray.length) * reverseValue}%`,
             duration: 1,
+            onComplete: () => {
+                setAnimating(false)
+            }
         })
     }
 
-        const handleNextSlide = () => {
+    const handleNextSlide = () => {
+        if (!animating) {
             if (currentSlide < itemsArray.length - 1) {
+                setAnimating(true)
                 setCurrentSlide(currentSlide + 1)
                 animateTriangle()
                 animateSlide()
             } else {
-                setCurrentSlide(0)
-                gsap.to('.slider-content', {
-                    x: 0,
-                    duration: 1,
-                })
+                return
             }
         }
+    }
 
-        const handlePrevSlide = () => {
+    const handlePrevSlide = () => {
+        if (!animating) {
             if (currentSlide > 0) {
+                setAnimating(true)
                 setCurrentSlide(currentSlide - 1)
                 animateTriangle(true)
                 animateSlide(true)
             } else {
-                setCurrentSlide(itemsArray.length - 1)
-                gsap.to('.slider-content', {
-                    x: `-${(100 / itemsArray.length) * (itemsArray.length - 1)}%`,
-                    duration: 1,
-                })
+                return
             }
         }
-
-        useGSAP(() => {
-
-            // const positionSlideSide = (after?: boolean) => {
-                
-
-            const beforeSlideTl = gsap.timeline()
-            if (imgRefs.current && txtRefs.current) {
-                beforeSlideTl.to(imgRefs.current.filter((_, index) => index === currentSlide - 1 || (currentSlide === 0 && index === itemsArray.length - 1)), {
-                    scale: .7,
-                    duration: 1,
-                    x: 180,
-                    filter: 'blur(5px)',
-                    y: 30,
-                    rotate: -20,
-                })
-                    .to(txtRefs.current.filter((_, index) => index === currentSlide - 1 || (currentSlide === 0 && index === itemsArray.length - 1)), {
-                        opacity: 0,
-                        duration: .2,
-                        y: 10,
-                    }, 0)
-
-            }
-
-            const afterSlideTl = gsap.timeline()
-            if (imgRefs.current && txtRefs.current) {
-                afterSlideTl.to(imgRefs.current.filter((_, index) => index === currentSlide + 1 || (currentSlide === itemsArray.length - 1 && index === 0)), {
-                    scale: .7,
-                    duration: 1,
-                    x: -180,
-                    y: 30,
-                    rotate: 20,
-                    filter: 'blur(5px)',
-                })
-                    .to(txtRefs.current.filter((_, index) => index === currentSlide + 1 || (currentSlide === itemsArray.length - 1 && index === 0)), {
-                        opacity: 0,
-                        duration: .2,
-                        y: 10,
-                    }, 0)
-
-            }
-
-            const activeSlideTl = gsap.timeline()
-            if (imgRefs.current && txtRefs.current) {
-                activeSlideTl.to(imgRefs.current[currentSlide], {
-                    scale: 1,
-                    duration: 1,
-                    filter: 'blur(0px)',
-                    x: 0,
-                    y: 0,
-                    rotate: 0,
-                })
-                    .to(txtRefs.current[currentSlide], {
-                        opacity: 1,
-                        duration: .5,
-                        y: 0,
-                    }, .5)
-            }
-
-            return [beforeSlideTl, activeSlideTl]
-
-        }, [currentSlide])
-
-        return (
-            <div className='slider-screen'>
-                <ArrowGalery onClick={handlePrevSlide} rotate fill='var(--c-dark)' />
-                <div className='slider-container'>
-                    <div className='slider-frame'>
-                        <SvgPointer fill={currentColor} />
-                        <div className='slider-content'>
-                            {itemsArray.map((item, index) => (
-                                <div
-                                    className='slider-item' key={index}>
-                                    <img ref={(element) => (imgRefs.current[index] = element)} src={item.img} />
-                                    <div ref={(element) => (txtRefs.current[index] = element)} className='slider-text'>
-                                        <p>PALESTRA</p>
-                                        <h3>{item.name}</h3>
-                                        <p>{item.content}</p>
-                                        <p>
-                                            <b>Conteúdo</b>
-                                            <br />
-                                            Segurança psicológica e atualização de identidade.
-                                        </p>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                <ArrowGalery onClick={handleNextSlide} fill='var(--c-dark)' />
-                <SvgBlob fill={currentColor} />
-            </div>
-        );
     }
 
-    export default SliderPalestra
+    useEffect(() => {
+        if (currentSlide === 0) {
+            gsap.set(imgRefs.current.filter((_, index) => index > currentSlide), {
+                scale: .7,
+                x: -180,
+                y: 30,
+                rotate: 20,
+                filter: 'blur(5px)',
+            })
+            gsap.set(txtRefs.current.filter((_, index) => index > currentSlide), {
+                opacity: 0,
+                duration: .2,
+                y: 10,
+            })
+        }
+    }, [])
+
+    useGSAP(() => {
+        const beforeSlideTl = gsap.timeline()
+        beforeSlideTl.to(imgRefs.current.filter((_, index) => index < currentSlide), {
+            scale: .7,
+            duration: 1,
+            x: 180,
+            filter: 'blur(5px)',
+            y: 30,
+            rotate: -20,
+        })
+            .to(txtRefs.current.filter((_, index) => index < currentSlide), {
+                opacity: 0,
+                duration: .2,
+                y: 10,
+            }, 0)
+
+
+        const afterSlideTl = gsap.timeline()
+        afterSlideTl.to(imgRefs.current.filter((_, index) => index > currentSlide), {
+            scale: .7,
+            duration: 1,
+            x: -180,
+            y: 30,
+            rotate: 20,
+            filter: 'blur(5px)',
+        })
+            .to(txtRefs.current.filter((_, index) => index > currentSlide), {
+                opacity: 0,
+                duration: .2,
+                y: 10,
+            }, 0)
+
+
+        const activeSlideTl = gsap.timeline()
+        activeSlideTl.to(imgRefs.current[currentSlide], {
+            scale: 1,
+            duration: 1,
+            filter: 'blur(0px)',
+            x: 0,
+            y: 0,
+            rotate: 0,
+        })
+            .to(txtRefs.current[currentSlide], {
+                opacity: 1,
+                duration: .5,
+                y: 0,
+            }, .8)
+
+    }, [currentSlide])
+
+    return (
+        <div className='slider-screen'>
+            <ArrowGalery onClick={handlePrevSlide} rotate fill='var(--c-dark)' />
+            <div className='slider-container'>
+                <div className='slider-frame'>
+                    <SvgPointer fill={currentColor} />
+                    <div ref={contentRef} className='slider-content'>
+                        {itemsArray.map((item, index) => (
+                            <div
+                                className='slider-item' key={index}>
+                                <img ref={(element) => (imgRefs.current[index] = element)} src={item.img} />
+                                <div ref={(element) => (txtRefs.current[index] = element)} className='slider-text'>
+                                    <p>PALESTRA</p>
+                                    <h3>{item.name}</h3>
+                                    <p>{item.content}</p>
+                                    <p>
+                                        <b>Conteúdo</b>
+                                        <br />
+                                        Segurança psicológica e atualização de identidade.
+                                    </p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <ArrowGalery onClick={handleNextSlide} fill='var(--c-dark)' />
+            <SvgBlob fill={currentColor} />
+        </div>
+    );
+}
+
+export default SliderPalestra
